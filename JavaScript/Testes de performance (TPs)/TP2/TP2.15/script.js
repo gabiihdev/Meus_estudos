1ee2478894ebe5b553964f9a165d3e9791e68448
Gabriela <gabiih.dev@gmail.com>
2024-09-10 17:21:29 -0300

    cálculo do ano bissexto com if else

---
+++ JavaScript/Testes de performance (TPs)/TP2/TP2.15/script.js
@@ -0,0 +1,7 @@
+let ano = prompt("Digite um ano com 4 dígitos:")
+
+if (ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0) {
+    alert(`O ano ${ano} é bissexto.`)
+} else {
+    alert(`O ano ${ano} não é bissexto.`)
+}
